b1262cdb2bd497ecbe53be8085fa920c18608c62
Neilketchum <daipayanh@gmail.com>
2020-09-09 11:11:23 +0530

    Deployment dynamic port1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require("express");
+const express = require('express')
 const app = express();
-app.get('/',(req,res)=>{
-    res.send({hi:"there"});
-});
-const PORT = process.env.PORT||5000
-app.listen(PORT);
+app.get('/', (req, res) => {
+    res.send({hi:'there'})
+  })
+const port = process.env.PORT || 8080;
+app.listen(port);
